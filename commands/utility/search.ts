a153a597bb5ea3eb3e3464153fd0551fd4a7ee5c
Nimbi <Darkwater409@gmail.com>
2021-03-08 19:02:38 -0700

    Update - 03/08/2021
     - Update `search.ts`

--- commands/utility/search.ts
+++ commands/utility/search.ts
@@ -1,18 +1,19 @@
 const API_KEY = ' ';
 let f = discord.command.filters;
-const QUERY_PERMS = f.and(f.canSendMessages());
 
-const SearchCommand = discord.interactions.commands;
+const QuerySlashCommand = discord.interactions.commands;
+const QueryCommand = new discord.command.CommandGroup({
+  defaultPrefix: 'p.'
+});
 
-SearchCommand.register(
+QuerySlashCommand.register(
   {
     name: 'search',
-    description: 'Search for something!',
-    filters: QUERY_PERMS,
+    description: 'Search with Wolfram Alpha API',
     options: (opt) => ({
       query: opt.string({
         name: 'query',
-        description: 'The thing to search for'
+        description: 'Search with Wolfram Alpha API'
       })
     })
   },
@@ -29,5 +30,38 @@ You searched for: \`${query}\`
 Result: ${res}
 
 (Done in ${Date.now() - s}ms)`);
+  }
+);
+
+QueryCommand.on(
+  {
+    name: 'search',
+    aliases: ['s', 'wolfram', 'ws', 'wa'],
+    description: 'Search with Wolfram Alpha API'
+  },
+  (args) => ({
+    query: args.text()
+  }),
+  async (message, { query }) => {
+    const appId = API_KEY;
+
+    let api = await fetch(
+      `http://api.wolframalpha.com/v1/result?appid=${appId}&i=${query}`
+    );
+    let result = await api.text();
+
+    const replyEmbed = new discord.Embed();
+    const description = `**Query:**
+    \`\`\`${query}\`\`\`
+    **Response:**
+    \`\`\`${result}\`\`\``;
+    replyEmbed.setDescription(description);
+    replyEmbed.setFooter({
+      text: `Powered by the WolframAlpha API`,
+      iconUrl:
+        'https://community.wolfram.com/group-branding-portlet/images/wa-asset.png'
+    });
+
+    await message.reply(replyEmbed);
   }
 );
