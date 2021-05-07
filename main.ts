f2c18fb53439897b02d4c847bdcd6eca53f30329
Nimbi <Darkwater409@gmail.com>
2021-05-07 00:52:27 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,17 @@
 import './modules/logger';
 import './modules/autopublisher';
 import './modules/functions';
+import './modules/siri';
 
 // Events
 import './events/message';
+import './events/error';
 
 // Admin Commands
 import './commands/admin/ban';
 import './commands/admin/banlist';
 import './commands/admin/kick';
 import './commands/admin/slowmode';
-
-// Economy Commands
-import './commands/economy/xp';
 
 // Mod Commands
 import './commands/mod/announce';
@@ -25,7 +24,10 @@ import './commands/social/twitter';
 import './commands/social/reddit';
 
 // Utility Commands
-import './commands/util/info'
-import './commands/util/menu'
-import './commands/util/ping'
-import './commands/util/search'
+import './commands/util/info';
+import './commands/util/menu';
+import './commands/util/ping';
+import './commands/util/search';
+
+// Economy Commands
+import './commands/economy/xp';
